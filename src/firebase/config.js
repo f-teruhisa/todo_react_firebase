68621963cc70fad5db7f5aaa510b7776ff9d8a32
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-01 20:42:48 +0900

    fix config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAilKEw1LbI_1AgCgAJ0txGvsGuMKh9FJo",
   authDomain: "todo-react-firebase-43af8.firebaseapp.com",
-  databaseURL: "https://todo-react-firebase-43af8.firebaseio.com",
+  databaseURL: "https://todo-react-firebase-43af8.firebaseio.com/",
   storageBucket: "todo-react-firebase-43af8.appspot.com",
   messagingSenderId: "534260090314",
   projectId: "todo-react-firebase-43af8"
