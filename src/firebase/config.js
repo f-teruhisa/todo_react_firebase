f5e7349385b8f2dfa457adda41b645cf248da36d
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-01 15:05:03 +0900

    add config firebase api key

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAilKEw1LbI_1AgCgAJ0txGvsGuMKh9FJo",
+  authDomain: "todo-react-firebase-43af8.firebaseapp.com",
+  databaseURL: "https://todo-react-firebase-43af8.firebaseio.com",
+  projectId: "todo-react-firebase-43af8",
+  storageBucket: "todo-react-firebase-43af8.appspot.com",
+  messagingSenderId: "534260090314"
+}
+
+export default firebaseConfig;
