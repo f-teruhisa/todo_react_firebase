ea63213d0b9ec54799a473522b4726a73d7e0a08
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-01 22:18:31 +0900

    implementation login view

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,48 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { isLoaded, isEmpty } from 'react-redux-firebase'
+import { loginWithGoogle, logout } from '../actions/authActions'
 
-const Login = () => (
-  ''
+let Login = ({ auth, loginWithGoogle, logout }) => {
+    if (!isLoaded(auth)) {
+      return ( <div> Now Login... </div>);
+      }
+      if (isEmpty(auth)) {
+        return (
+          <button onClick = {loginWithGoogle}>Login with Google Account</button>
+        )
+      }
+  return (
+    <div>
+      {auth.displayName} {}
+      <button onClick = {logout}>Logout</button>{}
+    </div>
+  );
+}
+
+Login.propTypes = {
+  auth: PropTypes.object.isRequired,
+  loginWithGoogle: PropTypes.func.isRequired,
+  logout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => (
+  { auth: state.firebase.auth }
 )
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loginWithGoogle: () => dispatch(loginWithGoogle()),
+    logout: () => {
+      dispatch(logout())
+    }
+  }
+}
+
+Login = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login)
 
 export default Login;
