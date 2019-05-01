10c90c516527f394305456605cd98625d1baa498
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-01 16:41:46 +0900

    fix getVisible Todos

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -6,15 +6,26 @@ import TodoList from '../components/TodoList'
 import { VisibilityFilters } from '../actions'
 
 const getVisibleTodos = (todos, filter) => {
+  if (!todos) return todos
   switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
+    case 'SHOW_ALL':
       return todos
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(t => t.completed)
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(t => !t.completed)
+    case 'SHOW_COMPLETED':
+      return Object.keys(todos)
+        .filter(key => todos[key].completed)
+        .reduce((filtered, key) => {
+          filtered[key] = todos[key];
+          return filtered;
+        }, {})
+    case 'SHOW_ACTIVE':
+      return Object.keys(todos)
+        .filter(key => !todos[key].completed)
+        .reduce((filtered, key) => {
+          filtered[key] = todos[key];
+          return filtered;
+        }, {})
     default:
-      throw new Error('Unknown filter: ' + filter)
+      return todos;
   }
 }
 
