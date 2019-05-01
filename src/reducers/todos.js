96d9df5b91a30b41ac0bc6ee648bca8978946081
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-01 17:57:51 +0900

    fix toggle method

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,3 +1,7 @@
+const getStringForCompleted = (completed) => ( // #2
+  completed ? '完了' : '未完了'
+)
+
 const todos = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO_REQUEST':
@@ -6,6 +10,27 @@ const todos = (state = {}, action) => {
       return {...state, notice: 'Complete'}
     case 'ADD_TODO_ERROR':
       return {...state, notice: 'Errror'}
+    case 'TOGGLE_TODO_REQUEST':
+      return {...state, notice:
+          '"' + action.text + '"\'s Status"' +
+          getStringForCompleted(action.completed) +
+          '"changing...'
+      }
+    case 'TOGGLE_TODO_SUCCESS':
+      return {
+        ...state,
+        notice: // #1
+          '"' + action.text + '"\'s Status"' +
+          getStringForCompleted(action.completed) +
+          '"cahnged.'
+      }
+    case 'TOGGLE_TODO_ERROR':
+      return {
+        ...state,
+        notice: // #1
+          '"' + action.text + '"error occured'
+      }
+      // case 'TOGGLE_TODO’: は削除  // #3
     default:
       return state
   }
