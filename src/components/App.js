10a9b0f8d2cc3013dbb566ccf5bda8c20ed6c8b4
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-02 18:01:26 +0900

    add react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './Login'
+import Navbar from './Navbar'
+import Dashboard from './Dashboard'
 import TodoComponent from './TodoComponent'
+import NoMatch from './NoMatch'
+
 
 const App = () => (
-  <div>
-    <Login />
-    <TodoComponent />
-  </div>
+  <BrowserRouter>
+    <div>
+      <Login />
+      <Navbar />
+      <Switch >
+        <Route exact path = "/" component = {Dashboard}/>
+        <Route exact path= "/users/:uid/todos" component = {TodoComponent}/>
+        <Route component= {NoMatch}/>
+      </Switch>
+    </div>
+  </BrowserRouter >
 )
 
 export default App;
