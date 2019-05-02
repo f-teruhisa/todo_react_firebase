ed605a9bd2e8042ae9f043a595f8b71675c8deb7
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-02 21:09:17 +0900

    updated task into recently tasks

--- functions/index.js
+++ functions/index.js
@@ -2,22 +2,26 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const addRecentUpdate = (uid, todoId, text, eventType) => {
+  return admin.database().ref('/users/' + uid + '/displayName').once('value').then((snapshot) => {
+    const displayName = snapshot.val();
+    return (admin.database().ref('/recentUpdatedTodos/' + todoId).set({
+      uid,
+      displayName,
+      text,
+      eventType,
+      _updatedAt: admin.database.ServerValue.TIMESTAMP
+    }));
+  });
+}
+
 exports.addRecentUpdateOnCreate = functions.database.ref('/todos/{uid}/{todoId}/text')
   .onCreate((snapshot, context) => {
     const uid = context.params.uid;
     const todoId = context.params.todoId;
     const text = snapshot.val();
 
-    return admin.database().ref('/users/' + uid + '/displayName').once('value').then((snapshot) => {
-      const displayName = snapshot.val();
-      return admin.database().ref('/recentUpdatedTodos/' + todoId).set({
-        uid,
-        displayName,
-        text,
-        eventType: 'CREATE',
-        _updatedAt: admin.database.ServerValue.TIMESTAMP
-      });
-    });
+    return addRecentUpdate(uid, todoId, text, 'CREATE');
   })
 
 exports.addRecentUpdateOnUpdateCompleted = functions.database.ref('/todos/{uid}/{todoId}/completed')
@@ -25,7 +29,8 @@ exports.addRecentUpdateOnUpdateCompleted = functions.database.ref('/todos/{uid}/
     const todoId = context.params.todoId;
     const uid = context.params.uid;
 
-    console.log('addRecentUpdateOnUpdateCompleted called. uid=' + uid + ', todoId=' + todoId);
-
-    return null;
+    return change.after.ref.parent.child('text').once('value').then((snapshot) => {
+      const text = snapshot.val();
+      return addRecentUpdate(uid, todoId, text, 'UPDATE');
+    });
   })
