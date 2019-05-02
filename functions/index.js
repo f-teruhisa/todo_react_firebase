52a4b4108e0488cd0bff1c77fcdfd2c3eb81de94
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-02 20:56:30 +0900

    add cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addRecentUpdateOnCreate = functions.database.ref('/todos/{uid}/{todoId}/text')
+  .onCreate((snapshot, context) => {
+    const uid = context.params.uid;
+    const todoId = context.params.todoId;
+
+    console.log('addRecentUpdateOnCreate called. uid=' + uid + ', todoId=' + todoId);
+
+    return null;
+  })
+
+exports.addRecentUpdateOnUpdateCompleted = functions.database.ref('/todos/{uid}/{todoId}/completed')
+  .onUpdate((change, context) => {
+    const todoId = context.params.todoId;
+    const uid = context.params.uid;
+
+    console.log('addRecentUpdateOnUpdateCompleted called. uid=' + uid + ', todoId=' + todoId);
+
+    return null;
+  })
