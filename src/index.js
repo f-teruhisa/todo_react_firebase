c0c784b2bae56ab0abb70cad0fc3418e4b1b5169
Teruhisa Fukumoto <teru_fukumoto@outlook.jp>
2019-05-03 16:17:20 +0900

    add registerServiceWorker.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import {
-  createStore,
-  applyMiddleware,
-  compose
-} from 'redux'
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
@@ -12,24 +8,24 @@ import firebase from 'firebase'
 import todoApp from './reducers'
 import App from './components/App';
 import firebaseConfig from './firebase/config'
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 import {MuiThemeProvider} from '@material-ui/core/styles'
 import {theme} from './materialui/theme'
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 const createStoreWithFirebase = compose(
   applyMiddleware(thunk.withExtraArgument({getFirebase})),
-  reactReduxFirebase(firebase, {userProfile: 'users',preserveOnLogout: ['todos', 'users', 'recentUpdatedTodos']})
-)(createStore);
+  reactReduxFirebase(firebase, {userProfile: 'users', preserveOnLogout: ['todos', 'users', 'recentUpdatedTodos']})
+)(createStore)
 
-const store = createStoreWithFirebase(todoApp);
+const store = createStoreWithFirebase(todoApp)
 
 render (
-  <Provider store={store}>
-    <MuiThemeProvider theme={theme}> 
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
       <App />
-    </MuiThemeProvider>
-  </Provider>,
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 )
